b7a7519d298fabee23c8314fb3a42e7000ea4a49
Illia Laziuk <illialaziuk2@gmail.com>
2021-04-05 12:14:56 +0300

    Change channel, now it works

--- buffered_channel.h
+++ buffered_channel.h
@@ -1,8 +1,8 @@
 #ifndef BUFFERED_CHANNEL_H_
 #define BUFFERED_CHANNEL_H_
 
-#include <stdio.h>
 #include <mutex>
+#include <condition_variable>
 #include <queue>
 
 template<class T>
@@ -13,33 +13,33 @@ class BufferedChannel {
     mutable std::mutex lockAdd;
     std::condition_variable qCheck;
     bool isOpen;
-    bool isEmpty;
 
   public:
-    explicit BufferedChannel(int size) : bufSize(size), isOpen(true), isEmpty(true) {}
+    explicit BufferedChannel(int size) : bufSize(size), isOpen(true) {}
 
     void Send(T value) {
       std::unique_lock<std::mutex> lock(lockAdd);
-      if(!isOpen) throw std::runtime_error("chanel is closed");
+      while(q.size() >= bufSize) qCheck.wait(lock);
+      if(!isOpen) throw std::runtime_error("channel is closed");
       q.push(value);
-      isEmpty = false;
+      lock.unlock();
       qCheck.notify_all();
     }
 
     std::pair<T, bool> Recv() {
       std::unique_lock<std::mutex> lock(lockAdd);
-      while(isEmpty) qCheck.wait(lockAdd);
+      while(q.empty()) qCheck.wait(lock);
       T el;
-      if(!q.empty()) {
-        el = q.front();
-        q.pop();
-      }
+      el = q.front();
+      q.pop();
+      lock.unlock();
       return std::make_pair(el, isOpen);
     }
 
     void Close() {
       std::unique_lock<std::mutex> lock(lockAdd);
       isOpen = false;
+      lock.unlock();
       qCheck.notify_all();
     }
 };
